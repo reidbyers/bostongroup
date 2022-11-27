4050f2089d8a741f3d2cd674a2b03e8093effcd8
Reid Byers <rbyers@datacollaborative.com>
2022-11-27 14:14:21 -0500

    first commit

---
+++ readFile.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+from bs4 import BeautifulSoup
+
+with open('29-home-8.xml', encoding='utf-8') as f:
+    file = f.read()
+
+soup = BeautifulSoup(file, 'xml')
+print(soup)
+
+segments = soup.find_all('segment')
+
+for i in range(len(segments)):
+    segment = segments[i]
+    print(segment)
+    
+    #print(unit)
